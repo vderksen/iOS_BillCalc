88bfbe26a52429ea9a29857f260d50d8d2ff72ed
vderksen <53282591+vderksen@users.noreply.github.com>
2021-02-02 15:26:54 -0500

    Initial Commit

---
+++ ValentinaDerksen_BillCalc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ValentinaDerksen_BillCalc
+//
+//  Created by Valya Derksen on 2021-02-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
